0a3bcfead05340beed2ac2e2c65177eee682362e
Jerry Mooyman <jamooyman@gmail.com>
2017-09-03 20:04:13 +1000

    used some immutable collections

--- app/src/main/java/com/mooyz/androidtictactoy/MainActivity.kt
+++ app/src/main/java/com/mooyz/androidtictactoy/MainActivity.kt
@@ -8,71 +8,49 @@ import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-import java.util.function.BiConsumer
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
 
-    val gameOver = false
     val NO_PLAYER = -1
     val PLAYER_1 = 1
     val PLAYER_2 = 2
 
-    val allCellNumbers = listOf<Int>(1,2,3,4,5,6,7,8,9)
+    val allCellNumbers = arrayListOf(1,2,3,4,5,6,7,8,9)
 
-    var buttonIdMap = HashMap<Int, Int>()
     var buttonMap = HashMap<Int, Button>()
-    val ROW_1 = ArrayList<Int>()
-    val ROW_2 = ArrayList<Int>()
-    val ROW_3 = ArrayList<Int>()
 
-    val COL_1 = ArrayList<Int>()
-    val COL_2 = ArrayList<Int>()
-    val COL_3 = ArrayList<Int>()
+    val ROW_1 = listOf(1,2,3)
+    val ROW_2 = listOf(4,5,6)
+    val ROW_3 = listOf(7,8,9)
 
-    val DIAG_1 = ArrayList<Int>()
-    val DIAG_2 = ArrayList<Int>()
+    val COL_1 = listOf(1,4,7)
+    val COL_2 = listOf(2,5,8)
+    val COL_3 = listOf(3,6,9)
+
+    val DIAG_1 = listOf(1,5,9)
+    val DIAG_2 = listOf(3,5,7)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // init local vars
-        buttonIdMap[R.id.bu1] = 1
-        buttonIdMap[R.id.bu2] = 2
-        buttonIdMap[R.id.bu3] = 3
-        buttonIdMap[R.id.bu4] = 4
-        buttonIdMap[R.id.bu5] = 5
-        buttonIdMap[R.id.bu6] = 6
-        buttonIdMap[R.id.bu7] = 7
-        buttonIdMap[R.id.bu8] = 8
-        buttonIdMap[R.id.bu9] = 9
-
-        buttonMap[1] = bu1
-        buttonMap[2] = bu2
-        buttonMap[3] = bu3
-        buttonMap[4] = bu4
-        buttonMap[5] = bu5
-        buttonMap[6] = bu6
-        buttonMap[7] = bu7
-        buttonMap[8] = bu8
-        buttonMap[9] = bu9
-
-        ROW_1.addAll(listOf(1,2,3))
-        ROW_2.addAll(listOf(4,5,6))
-        ROW_3.addAll(listOf(7,8,9))
-
-        COL_1.addAll(listOf(1,4,7))
-        COL_2.addAll(listOf(2,5,8))
-        COL_3.addAll(listOf(3,6,9))
-
-        DIAG_1.addAll(listOf(1,5,9))
-        DIAG_2.addAll(listOf(3,5,7))
+        buttonMap = hashMapOf(
+                1 to bu1,
+                2 to bu2,
+                3 to bu3,
+                4 to bu4,
+                5 to bu5,
+                6 to bu6,
+                7 to bu7,
+                8 to bu8,
+                9 to bu9
+        )
     }
 
-    var player1Picks = ArrayList<Int>()
-    var player2Picks = ArrayList<Int>()
+    var player1Picks = arrayListOf<Int>()
+    var player2Picks = arrayListOf<Int>()
     var ActivePlayer = PLAYER_1
 
     fun buClick(view: View) {
@@ -87,7 +65,7 @@ class MainActivity : AppCompatActivity() {
         if (ActivePlayer == PLAYER_1) {
             playerMove(cellId, buSelected, player1Picks, Color.GREEN, "X")
             winningPlayer = if (checkWinner(player1Picks)) PLAYER_1 else NO_PLAYER
-            if (!getEmptyCells(player1Picks, player2Picks).isEmpty()) {
+            if (getEmptyCells(player1Picks, player2Picks, allCellNumbers).isNotEmpty()) {
                 ActivePlayer = PLAYER_2
             }
         } else {
@@ -95,8 +73,6 @@ class MainActivity : AppCompatActivity() {
             winningPlayer = if (checkWinner(player2Picks)) PLAYER_2 else NO_PLAYER
             ActivePlayer = PLAYER_1
         }
-
-        buSelected.isEnabled = false
 
         if (winningPlayer != NO_PLAYER) {
             Toast.makeText(this, "Player $winningPlayer wins the game", Toast.LENGTH_LONG).show()
@@ -113,6 +89,7 @@ class MainActivity : AppCompatActivity() {
     fun playerMove(cellId: Int, buSelected: Button, playerPicks: ArrayList<Int>, color: Int, playerMark: String) {
         buSelected.text = playerMark
         buSelected.setBackgroundColor(color)
+        buSelected.isEnabled = false
         playerPicks.add(cellId)
     }
 
@@ -122,11 +99,11 @@ class MainActivity : AppCompatActivity() {
                 || hasRun(playerPicks, DIAG_1) || hasRun(playerPicks, DIAG_2)
     }
 
-    fun hasRun(playerPicks: ArrayList<Int>, cellsForRun: ArrayList<Int>): Boolean {
+    fun hasRun(playerPicks: List<Int>, cellsForRun: List<Int>): Boolean {
         return playerPicks.containsAll(cellsForRun)
     }
 
-    fun getEmptyCells(player1Picks:ArrayList<Int>, player2Picks:ArrayList<Int>):ArrayList<Int> {
+    fun getEmptyCells(player1Picks:List<Int>, player2Picks:List<Int>, allCellNumbers:List<Int>):List<Int> {
         var selectedCells = ArrayList<Int>()
         selectedCells.addAll(player1Picks)
         selectedCells.addAll(player2Picks)
@@ -141,8 +118,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buttonIdToCell(buttonId:Int):Int {
-        val r = buttonIdMap[buttonId]
-        return if (r != null) return r else throw Exception("Invalid buttonId: $buttonId")
+        val s = buttonMap.filter { entry -> entry.value.id == buttonId }
+        return if (s.size == 1) return s.keys.first() else throw Exception("Invalid buttonId: $buttonId")
     }
 
     fun cellIdToButton(cellId:Int):Button {
@@ -151,7 +128,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun autoPlay() {
-        val emptyCells = getEmptyCells(player1Picks, player2Picks)
+        val emptyCells = getEmptyCells(player1Picks, player2Picks, allCellNumbers)
 
         val randomIndex = Random().nextInt(emptyCells.size-0)+0
         val cellId = emptyCells[randomIndex]
